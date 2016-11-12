82cec356af237035c0ee88ea5784b05c56d3336e
Zech Xu <zhenjiang.xu@gmail.com>
2016-11-12 11:10:59 -0800

    implement filtering functions

--- calour/filtering.py
+++ calour/filtering.py
@@ -1,55 +1,192 @@
-# calour functions for filtering samples/observations
-# functions should call reorder_samples() / reorder_obs()
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016--,  Calour development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+# ----------------------------------------------------------------------------
+
+from statistics import mean
+from heapq import nlargest
+from logging import getLogger
+
+import numpy as np
 
 
-def filter_samples(exp, field, values, exclude=False, inplace=False):
-	'''
-	filter samples from the experiment keeping only samples where the value in field
-	is in values
-	if exclude is true - remove the matching samples instead of filtering them
-	'''
-
-	# should call reorder_samples()
+logger = getLogger(__name__)
 
 
-def filter_orig_reads(exp, minreads=10000, inplace=False):
-	'''
-	filter samples that have less than minreads total reads
-	'''
+def down_sample(exp, field, axis=0, inplace=False):
+    '''Down sample the data set.
+
+    This down samples all the samples to have the same number of
+    samples for each categorical value of the field in
+    ``sample_metadata`` or ``feature_metadata``.
+
+    Parameters
+    ----------
+    field : str
+        The name of the column in samples metadata table. This column
+        should has categorical values
+
+    Returns
+    -------
+
+    '''
+    if axis == 0:
+        x = exp.sample_metadata
+    elif axis == 1:
+        x = exp.feature_metadata
+    values = x[field].values
+    unique, counts = np.unique(values, return_counts=True)
+    min_index = counts.argmin()
+    min_value = unique[min_index]
+    min_count = counts[min_index]
+    indices = []
+    for i in unique:
+        i_indice = np.where(values == i)[0]
+        if i == min_value:
+            indices.append(i_indice)
+        else:
+            indices.append(np.random.choice(i_indice, min_count))
+    return exp.reorder(np.concatenate(indices), axis=axis, inplace=inplace)
 
 
-def filter_taxonomy(exp, taxonomy, exact=False, exclude=False, inplace=False):
-	'''
-	filter keeping only observations with taxonomy string matching taxonomy
-	if exact=True, look for partial match instead of identity
-	'''
+def filter_by_metadata(exp, field, values, axis=0, negate=False, inplace=False):
+    '''Filter samples or features by metadata.
+    '''
+    logger.info('')
+    if axis == 0:
+        x = exp.sample_metadata
+    elif axis == 1:
+        x = exp.feature_metadata
+    select = x[field].isin(values).values()
+    if negate is True:
+        select = ~ select
+    return exp.reorder(select, axis=axis, inplace=inplace)
 
 
-def filter_obs_min_reads(exp, minreads=10, exclude=False, inplace=False):
-	'''
-	keep only observations with at least minreads total over all samples
-	'''
+def filter_by_data(exp, predicate, axis=0, negate=False, inplace=False):
+    '''Filter samples or features by data.
+
+    Parameters
+    ----------
+    predicate : str or callable
+        It accepts a list of numeric and return a bool.
+    axis : 0 or 1
+        Apply predicate on row or column
+    negate : bool
+        negate the predicate for selection
+    '''
+    logger.info('')
+    func = {'min_abundance': _min_abundance,
+            'freq_ratio': _freq_ratio,
+            'unique_cut': _unique_cut,
+            'mean_abundance': _mean_abundance,
+            'presence_fraction': _presence_fraction}
+    if isinstance(predicate, str):
+        predicate = func[predicate]
+    select = np.apply_along_axis(predicate, axis, exp.data)
+    if negate is True:
+        select = ~ select
+    return exp.reorder(select, axis=axis, inplace=inplace)
 
 
-def filter_obs_mean_reads(exp, meanreads=0.001, exclude=False, inplace=False):
-	'''
-	keep only observations with at least minreads mean in all samples
-	'''
+def filter_taxonomy(exp, taxonomy, exact=False, negate=False, inplace=False):
+    '''
+    filter keeping only observations with taxonomy string matching taxonomy
+    if exact=True, look for partial match instead of identity
+    '''
 
 
-def filter_obs_presence(exp, frac_pres=0.5, threshold=0, exclude=False, inplace=False):
-	'''
-	keep only observations present (number reads > threshold) in at least frac_pres of samples
-	'''
+def _min_abundance(x, cutoff=10):
+    '''Check if the min abundance larger than cutoff.
+
+    It can be used filter features with at least "cutoff" abundance
+    total over all samples
+
+    Examples
+    --------
+    >>> _min_abundance([0, 1, 1], 2)
+    True
+    >>> _min_abundance([0, 1, 1], 2.01)
+    False
+
+    '''
+    logger.debug('')
+    return sum(x) >= cutoff
 
 
-def filter_obs(exp, values, exclude=False, inplace=False):
-	'''
-	keep only observations with id (usually sequence) in values list
-	'''
+def _mean_abundance(x, cutoff=0.01):
+    '''Check if the mean abundance larger than cutoff.
+
+    Can be used to keep features with means at least "cutoff" in all
+    samples
+
+    Examples
+    --------
+    >>> _mean_abundance([0, 0, 1, 1], 0.51)
+    False
+    >>> _mean_abundance([0, 0, 1, 1], 0.5)
+    True
+
+    '''
+    logger.debug('')
+    return mean(x) >= cutoff
 
 
-def filter_seqs_from_fasta(exp, filename, exclude=False, inplace=False):
-	'''
-	keep only observations who'se id (sequence) is in the fasta file filename
-	'''
+def _presence_fraction(x, fraction=0.5, cutoff=0):
+    '''Check the presence fraction.
+
+    present (abundance >= cutoff) in at least "fraction" of samples
+
+    Examples
+    --------
+    >>> _presence_fraction([0, 1])
+    True
+    >>> _presence_fraction([0, 1, 2, 3], 0.5, 2)
+    True
+    >>> _presence_fraction([0, 1, 2], 0.51, 2)
+    False
+    '''
+    logger.debug('')
+    frac = sum(i >= cutoff for i in x) / len(x)
+    return frac >= fraction
+
+
+def _unique_cut(x, unique=0.05):
+    '''the percentage of distinct values out of the number of total samples.
+
+    Examples
+    --------
+    >>> _unique_cut([0, 0], 0.49)
+    True
+    >>> _unique_cut([0, 0], 0.51)
+    False
+    >>> _unique_cut([0, 1], 1.01)
+    False
+    '''
+    logger.debug('')
+    count = len(set(x))
+    return count / len(x) >= unique
+
+
+def _freq_ratio(x, ratio=2):
+    '''the ratio of the most common value to the second most common value
+
+    Return True if the ratio is not greater than "ratio".
+
+    Examples
+    --------
+    >>> _freq_ratio([0, 0, 1, 2], 2)
+    True
+    >>> _freq_ratio([0, 0, 1, 1], 1.01)
+    True
+    >>> _freq_ratio([0, 0, 1, 2], 1.99)
+    False
+    '''
+    logger.debug('')
+    unique, counts = np.unique(np.array(x), return_counts=True)
+    max_1, max_2 = nlargest(2, counts)
+    return max_1 / max_2 <= ratio
+
