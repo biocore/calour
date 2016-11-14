007ea1e60fd2cd3b927e36cf90901201c5c8c430
amnona <amnonim@gmail.com>
2016-11-14 13:12:13 -0800

    unit test for io.py

---
+++ calour/tests/test_io.py
@@ -0,0 +1,55 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016--,  Calour development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+# ----------------------------------------------------------------------------
+
+import unittest
+from os.path import join, dirname, abspath
+
+import calour as ca
+
+
+class TestIO(unittest.TestCase):
+    def setUp(self):
+        self.test_data_dir = join(dirname(abspath(__file__)), 'data')
+        self.test_simple_table = join(self.test_data_dir, 'test1.biom.txt')
+        self.test_simple_map = join(self.test_data_dir, 'test1.map.txt')
+        self.test_complex_table = join(self.test_data_dir, 'timeseries.biom')
+        self.test_complex_map = join(self.test_data_dir, 'timeseries.map.txt')
+
+    def test_read(self):
+        # load the simple dataset as sparse
+        exp = ca.read(self.test_simple_table, self.test_simple_map)
+        # number of bacteria is 12
+        self.assertEqual(exp.data.shape[1],12)
+        # number of samples is 20 (should not read the samples only in map or only in biom table)
+        # self.assertEqual(exp.data.shape[0],20)
+        # test an OTU/sample to see it is in the right place
+        sseq = 'TACGTAGGGTGCAAGCGTTAATCGGAATTACTGGGCGTAAAGCGTGCGCAGGCGGTTTTGTAAGTCTGATGTGAAATCCCCGGGCTCAACCTGGGAATTGCATTGGAGACTGCAAGGCTAGAATCTGGCAGAGGGGGGTAGAATTCCACG'
+        ssample = 'S6'
+        self.assertIn(sseq, exp.feature_metadata.index)
+        self.assertIn(ssample, exp.sample_metadata.index)
+        samplepos = exp.sample_metadata.index.get_loc(ssample)
+        seqpos = exp.feature_metadata.index.get_loc(sseq)
+        self.assertEqual(exp.data[samplepos, seqpos], 6)
+
+        # load the simple dataset as dense
+        exp = ca.read(self.test_simple_table, self.test_simple_map, sparse=False)
+        # number of bacteria is 12
+        self.assertEqual(exp.data.shape[1],12)
+        # number of samples is 20 (should not read the samples only in map or only in biom table)
+        # self.assertEqual(exp.data.shape[0],20)
+        # test an OTU/sample to see it is in the right place
+        sseq = 'TACGTAGGGTGCAAGCGTTAATCGGAATTACTGGGCGTAAAGCGTGCGCAGGCGGTTTTGTAAGTCTGATGTGAAATCCCCGGGCTCAACCTGGGAATTGCATTGGAGACTGCAAGGCTAGAATCTGGCAGAGGGGGGTAGAATTCCACG'
+        ssample = 'S6'
+        self.assertIn(sseq, exp.feature_metadata.index)
+        self.assertIn(ssample, exp.sample_metadata.index)
+        samplepos = exp.sample_metadata.index.get_loc(ssample)
+        seqpos = exp.feature_metadata.index.get_loc(sseq)
+        self.assertEqual(exp.data[samplepos, seqpos], 6)
+
+if __name__ == "__main__":
+    unittest.main()
