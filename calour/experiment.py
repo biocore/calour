ba1448129f7defe40f589cc27b56514027ade76a
amnona <amnonim@gmail.com>
2016-11-13 19:09:26 -0800

    more functions

--- calour/experiment.py
+++ calour/experiment.py
@@ -131,7 +131,7 @@ class Experiment:
         return exp
 
 
-def add_functions(cls, modules=['.io', '.sorting', '.filtering', '.utils']):
+def add_functions(cls, modules=['.io', '.sorting', '.filtering', '.utils', '.normalization']):
     '''Dynamically add functions to the class as methods.'''
     for module_name in modules:
         module = import_module(module_name, 'calour')
