d9a243492ba86828b831f1f7b7298ce18e798dbf
Zech Xu <zhenjiang.xu@gmail.com>
2017-02-14 13:37:19 -0800

    add more tests and update filter_by_metadata API

--- calour/tests/test_filtering.py
+++ calour/tests/test_filtering.py
@@ -38,51 +38,111 @@ class FilteringTests(Tests):
         exp = self.test2.reorder([all_sid.index(i) for i in sid], axis=1)
         self.assertEqual(obs, exp)
 
-    def test_filter_by_metadata_sample(self):
-        obs = self.exp1.filter_by_metadata('group', 1)
-        exp = ca.read(*[get_data_path(i) for i in [
-            'filter.2.biom', 'filter.2_sample.txt']])
-        self.assertIsNot(obs, exp)
-        assert_experiment_equal(obs, exp)
+    def test_filter_by_metadata_sample_edge_cases(self):
+        # no group 3 - none filtered
+        obs = self.test2.filter_by_metadata('group', 3)
+        self.assertEqual(obs.shape, (0, 8))
+        obs = self.test2.filter_by_metadata('group', 3, negate=True)
+        assert_experiment_equal(obs, self.test2)
 
-        obs = self.exp1.filter_by_metadata('group', 1, inplace=True)
-        self.assertIs(obs, self.exp1)
+        # all samples are filtered
+        obs = self.test2.filter_by_metadata('group', [1, 2])
+        assert_experiment_equal(obs, self.test2)
+        obs = self.test2.filter_by_metadata('group', [1, 2], negate=True)
+        self.assertEqual(obs.shape, (0, 8))
+
+    def test_filter_by_metadata_sample(self):
+        for sparse, inplace in [(True, False), (True, True), (False, False), (False, True)]:
+            test2 = ca.read(self.test2_biom, self.test2_samp, self.test2_feat, sparse=sparse)
+            # only filter samples with id bewtween 3 and 7.
+            obs = test2.filter_by_metadata(
+                'id', lambda l: [7 > i > 3 for i in l], inplace=inplace)
+            self.assertEqual(obs.shape, (3, 8))
+            self.assertEqual(obs.sample_metadata.index.tolist(),
+                             ['S4', 'S5', 'S6'])
+            if inplace:
+                self.assertIs(obs, test2)
+            else:
+                self.assertIsNot(obs, test2)
+
+    def test_filter_by_metadata_feature_edge_cases(self):
+        # none filtered
+        obs = self.test2.filter_by_metadata('oxygen', 'facultative', axis=1)
+        self.assertEqual(obs.shape, (9, 0))
+        obs = self.test2.filter_by_metadata('oxygen', 'facultative', axis=1, negate=True)
+        assert_experiment_equal(obs, self.test2)
 
     def test_filter_by_metadata_feature(self):
-        obs = self.exp1.filter_by_metadata('taxonomy', 'bad_bacteria', negate=True, axis=1)
-        self.assertIsNot(obs, self.exp1)
-        exp = ca.read(*[get_data_path(i) for i in ['filter.3.biom', 'filter.1_sample.txt']])
-        assert_experiment_equal(obs, exp)
+        for sparse, inplace in [(True, False), (True, True), (False, False), (False, True)]:
+            test2 = ca.read(self.test2_biom, self.test2_samp, self.test2_feat, sparse=sparse)
+            # only filter samples with id bewtween 3 and 7.
+            obs = test2.filter_by_metadata(
+                'oxygen', 'anaerobic', axis=1, inplace=inplace)
+            self.assertEqual(obs.shape, (9, 2))
+            self.assertEqual(
+                obs.feature_metadata.index.tolist(),
+                ['TACGTAGGGCGCGAGCGTTATCCGGAATTATTGGGCGTAAAGAGTGCGTA'
+                 'GGTGGCATCTTAAGCGCAGGGTTTAAGGCAATGGCTCAACCATTGTTCGC'
+                 'CTTGCGAACTGGGGTGCTTGAGTGCAGGAGGGGAAAGTGGAATTCCTAGT',
+                 'AAAAAAAGGTCCAGGCGTTATCCGGATTTATTGGGTTTAAAGGGAGCGTA'
+                 'GGCGGACGATTAAGTCAGCTGCGAAAGTTTGCGGCTCAACCGTAAAATTG'
+                 'CAGTTGAAACTGGTTGTCTTGAGTGCACGCAGGGATGTTGGAATTCATGG'])
+            if inplace:
+                self.assertIs(obs, test2)
+            else:
+                self.assertIsNot(obs, test2)
 
-        obs = self.exp1.filter_by_metadata(
-            'taxonomy', 'bad_bacteria', axis=1, negate=True, inplace=True)
-        self.assertIs(obs, self.exp1)
+    def test_filter_by_data_sample_edge_cases(self):
+        # all samples are filtered out
+        obs = self.test2.filter_by_data('sum_abundance', cutoff=100000)
+        self.assertEqual(obs.shape, (0, 8))
+        # none is filtered out
+        obs = self.test2.filter_by_data('sum_abundance', cutoff=1)
+        assert_experiment_equal(obs, self.test2)
+        self.assertIsNot(obs, self.test2)
 
     def test_filter_by_data_sample(self):
-        for e in [self.exp1, self.exp2]:
-            # all samples are filtered out
-            obs = e.filter_by_data('sum_abundance', cutoff=10000)
-            self.assertEqual(obs.data.shape[0], 0)
-
-            obs = e.filter_by_data('sum_abundance', cutoff=1200)
-            exp = ca.read(*[get_data_path(i) for i in ['filter.5.biom', 'filter.5_sample.txt']])
+        for sparse, inplace in [(True, False), (True, True), (False, False), (False, True)]:
+            test2 = ca.read(self.test2_biom, self.test2_samp, self.test2_feat, sparse=sparse)
+            # filter out samples with abundance < 1200. only the last sample is filtered out.
+            obs = test2.filter_by_data(
+                'sum_abundance', axis=0, inplace=inplace, cutoff=1200)
+            self.assertEqual(obs.shape, (8, 8))
+            exp = ca.read(*[get_data_path(i) for i in [
+                'test2.biom.filter.sample',
+                'test2.sample.filter.sample',
+                'test2.feature']])
             assert_experiment_equal(obs, exp)
+            if inplace:
+                self.assertIs(obs, test2)
+            else:
+                self.assertIsNot(obs, test2)
 
-    def test_filter_by_data_feature(self):
+    def test_filter_by_data_feature_edge_cases(self):
         # all features are filtered out
         obs = self.test2.filter_by_data('sum_abundance', axis=1, cutoff=10000)
         self.assertEqual(obs.shape, (9, 0))
+
         # none is filtered out
         obs = self.test2.filter_by_data('sum_abundance', axis=1, cutoff=1)
         assert_experiment_equal(obs, self.test2)
-        # one feature is filtered out
-        obs = self.test2.filter_by_data('sum_abundance', axis=1, cutoff=25)
-        self.assertEqual(obs.shape, (9, 7))
-        exp = ca.read(*[get_data_path(i) for i in [
-            'test2.biom.filter.feature',
-            'test2.sample.filter.feature',
-            'test2.feature.filter.feature']])
-        assert_experiment_equal(obs, exp)
+        self.assertIsNot(obs, self.test2)
+
+    def test_filter_by_data_feature(self):
+        # one feature is filtered out when cutoff is set to 25
+        for sparse, inplace in [(True, False), (True, True), (False, False), (False, True)]:
+            obs = self.test2.filter_by_data(
+                'sum_abundance', axis=1, inplace=inplace, cutoff=25)
+            self.assertEqual(obs.shape, (9, 7))
+            exp = ca.read(*[get_data_path(i) for i in [
+                'test2.biom.filter.feature',
+                'test2.sample.filter.feature',
+                'test2.feature.filter.feature']])
+            assert_experiment_equal(obs, exp)
+            if inplace:
+                self.assertIs(obs, self.test2)
+            else:
+                self.assertIsNot(obs, self.test2)
 
 
 if __name__ == '__main__':
