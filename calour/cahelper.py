7fad239f89f522305a5cdc1c4f7b037341ed979e
amnona <amnonim@gmail.com>
2017-02-21 13:20:34 -0800

    fix pep8

--- calour/cahelper.py
+++ calour/cahelper.py
@@ -6,7 +6,6 @@
 # The full license is in the file COPYING.txt, distributed with this software.
 # ----------------------------------------------------------------------------
 import numpy as np
-import copy
 from logging import getLogger
 import skbio
 
