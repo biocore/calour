b6939c9abe2ad35aba6bf29270b6c59c17ba948a
Zech Xu <zhenjiang.xu@gmail.com>
2017-03-04 11:47:10 -0800

    fix publish.sh

--- publish.sh
+++ publish.sh
@@ -6,11 +6,11 @@ if [ "${TRAVIS_SECURE_ENV_VARS}" == "true" ] && [ "${CONDA_PY}" == "35" ] && [ "
     # clear the old html if there is any
     make -C doc clean
     # clone the gh-pages branch so we can keep its commit history
-    git clone -b gh-pages --single-branch https://$GH_TOKEN@github.com/biocore/calour.git doc/_build/html
+    git clone -b gh-pages --single-branch https://$GH_TOKEN@github.com/biocore/calour.git doc/build/html
     # update the html doc by build new doc
     make -C doc html
     # go to the html dir to commit changes and upload
-    cd doc/_build/html
+    cd doc/build/html
     git status
     # add and commit all changes
     git add *
