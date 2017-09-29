8f0cc07fb497115e365da24c1a1e4dc977ee290d
Jamie Morton <jamietmorton@gmail.com>
2017-09-28 23:07:05 -0400

    fixing dict update

--- calour/heatmap/heatmap.py
+++ calour/heatmap/heatmap.py
@@ -284,7 +284,8 @@ def _ax_color_bar(ax, values, width, position=0, colors=None, axis=0, label=True
 
     default_kwargs = {'color': 'w', 'weight': 'bold', 'fontsize': 7,
                       'ha': 'center', 'va': 'center'}
-    label_kwargs = default_kwargs(label_kwargs)
+    default_kwargs.update(label_kwargs)
+    label_kwargs =  default_kwargs
 
     uniques = np.unique(values)
     if colors is None:
