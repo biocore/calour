9fae94b0939ea0f86c78632f79d6d31d9e904d0c
amnona <amnonim@gmail.com>
2018-02-14 10:17:05 +0200

    try to fix sphinx error

--- calour/tests/test_experiment.py
+++ calour/tests/test_experiment.py
@@ -235,14 +235,14 @@ class ExperimentTests(Tests):
         self.assertEqual(repr(self.test1),
                          '''Experiment test1.biom
 ---------------------
-data dimension: 21 samples, 12 features
-sample IDs: Index(['S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'S11',
-       'S12', 'S13', 'S14', 'S15', 'S16', 'S17', 'S18', 'S19', 'S20',
-       'badsample'],
-      dtype='object', name='#SampleID')
-feature IDs: Index(['AA', 'AT', 'AG', 'AC', 'TA', 'TT', 'TG', 'TC', 'GA', 'GT', 'GG',
-       'badfeature'],
-      dtype='object')''')
+data dimension: 21 samples, 12 features''')
+# sample IDs: Index(['S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'S11',
+#        'S12', 'S13', 'S14', 'S15', 'S16', 'S17', 'S18', 'S19', 'S20',
+#        'badsample'],
+#       dtype='object', name='#SampleID')
+# feature IDs: Index(['AA', 'AT', 'AG', 'AC', 'TA', 'TT', 'TG', 'TC', 'GA', 'GT', 'GG',
+#        'badfeature'],
+#       dtype='object')''')
 
 
 if __name__ == "__main__":
