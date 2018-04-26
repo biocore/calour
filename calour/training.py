37e3a20b6e82b968007b0c4195c69d95145ea6e8
Zech Xu <zhenjiang.xu@gmail.com>
2018-04-26 11:21:31 -0700

    add functions for classification (#98)
    * fix a bug of colors x/y bars for heatmap
    
    If a dict given to `barx_colors`, the dict was converted to list of
    its keys and color info was lost in bar plotting. The problem is that
    `_to_list` should convert a dict to a list of dict.
    
    * add classify_cv
    
    * refactor classify_cv
    
    add plot_cm and plot_roc
    
    * add tests
    
    * make tests verbose
    
    * comment out plt.show()
    
    * improve doc
    
    * move pyplot

--- calour/training.py
+++ calour/training.py
@@ -14,11 +14,15 @@ Functions
    add_sample_metadata_as_features
 '''
 
-
 from logging import getLogger
+import itertools
 
 from sklearn.feature_extraction import DictVectorizer
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, RepeatedStratifiedKFold, ParameterGrid
+from sklearn.model_selection._split import check_cv
+from sklearn.base import is_classifier
+from sklearn.metrics import roc_curve, auc, confusion_matrix
+from scipy import interp
 from scipy.sparse import hstack
 import pandas as pd
 import numpy as np
@@ -121,24 +125,219 @@ def split_train_test(exp: Experiment, field, test_size, train_size=None, stratif
     return train_X, test_X, train_y, test_y
 
 
-@Experiment._record_sig
-def collect_cv_prediction(exp: Experiment, field, estimator, cv, scoring, inplace=False):
-    '''Do the CV
+def classify(exp: Experiment, field, estimator, cv=RepeatedStratifiedKFold(3, 1),
+             predict='predict_proba', param_grid=None):
+    '''Evaluate classification during cross validation.
+
+    Parameters
+    ----------
+    field : str
+        column name in the sample metadata, which contains the classes we want to predict.
+    estimator : object
+        scikit-learn estimator. e.g. :class:`sklearn.ensemble.RandomForestClassifer`
+    cv : int, cross-validation generator or an iterable
+        similar to the `cv` parameter in :class:`sklearn.model_selection.GridSearchCV`
+    predict : {'predict', 'predict_proba'}
+        the function used to predict the validation sets. Some estimators
+        have both functions to predict class or predict the probablity of each class
+        for a sample. For example, see :class:`sklearn.ensemble.RandomForestClassifier`
+    param_grid : dict of string to sequence, or sequence of such
+        the parameter passing to :class:`sklearn.model_selection.ParameterGrid`. By default,
+        it uses whatever default parameter of the `estimator` set in `scikit-learn`
+
+    Yields
+    ------
+    pandas.DataFrame
+        The result of prediction per sample for a given parameter set. It contains the
+        following columns:
+        - Y_TRUE: the true class for the samples
+        - SAMPLE: sample IDs
+        - CV: which split of the cross validation
+        - Y_PRED: the predicted class for the samples (if "predict")
+        - mutliple columns with each contain probabilities predicted as each class (if "predict_proba")
+    '''
+    X = exp.data
+    y = exp.sample_metadata[field]
+    cv = check_cv(cv, y, classifier=is_classifier(estimator))
+
+    if param_grid is None:
+        # use sklearn default param values for the given estimator
+        param_grid = {}
+
+    for param in ParameterGrid(param_grid):
+        logger.debug('run classification with parameters: %r' % param)
+        dfs = []
+        for i, (train, test) in enumerate(cv.split(X, y)):
+            model = estimator(**param)
+            model.fit(X[train], y[train])
+            pred = getattr(model, predict)(X[test])
+            if pred.ndim > 1:
+                df = pd.DataFrame(pred, columns=model.classes_)
+            else:
+                df = pd.DataFrame(pred, columns=['Y_PRED'])
+            df['Y_TRUE'] = y[test].values
+            df['SAMPLE'] = y[test].index.values
+            df['CV'] = i
+            dfs.append(df)
+        yield pd.concat(dfs, axis=0).reset_index(drop=True)
+
+
+def plot_cm(result, normalize=False, title='confusion matrix', cmap=None, ax=None):
+    '''Plot confusion matrix
+
+    Parameters
+    ----------
+    result : pandas.DataFrame
+        data frame containing predictions per sample (in row). It must have a column of
+        true class named "Y_TRUE". It must have a column of predicted class named "Y_PRED"
+        or multiple columns of predicted probabilities for each class. It typically takes
+        the output of :func:`classify`.
+    normalize : bool
+        normalize the confusion matrix or not
+    title : str
+        plot title
+    cmap : str or matplotlib.colors.ListedColormap
+        str to indicate the colormap name. Default is "Blues" colormap.
+        For all available colormaps in matplotlib: https://matplotlib.org/users/colormaps.html
+    ax : matplotlib.axes.Axes or None (default), optional
+        The axes where the confusion matrix is plotted. None (default) to create a new figure and
+        axes to plot the confusion matrix
+
+    Returns
+    -------
+    matplotlib.axes.Axes
+        The axes for the confusion matrix
 
     '''
-    # from sklearn.model_selection._split import check_cv
-    # from sklearn.base import is_classifier
-    # logger.debug('')
-    # if inplace:
-    #     new = exp
-    # else:
-    #     new = exp.copy()
-    # cv = check_cv(cv, y, classifier=is_classifier(estimator))
-    # for params in paramgrid:
-    #     for train_x, train_y in cv:
-    #         estimator.fit(train_x, train_y)
+    from matplotlib import pyplot as plt
+    if cmap is None:
+        cmap = plt.cm.Blues
+    classes = result['Y_TRUE'].unique()
+    cm = _compute_cm(result, labels=classes)
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        logger.debug("Normalized confusion matrix")
+    else:
+        logger.debug('Confusion matrix, without normalization')
+    if ax is None:
+        fig, ax = plt.subplots()
+    else:
+        fig = ax.get_figure()
+    img = ax.imshow(cm, cmap=cmap)
+    ax.set_title(title)
+    fig.colorbar(img)
+    tick_marks = np.arange(len(classes))
+    ax.tick_params(rotation=45)
+    ax.set_xticks(tick_marks)
+    ax.set_xticklabels(classes)
+    ax.set_yticks(tick_marks)
+    ax.set_yticklabels(classes)
 
-    # return yobs, yhat, model
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        ax.text(j, i, format(cm[i, j], fmt),
+                horizontalalignment="center",
+                color="white" if cm[i, j] > thresh else "black")
+
+    ax.set_ylabel('Observation')
+    ax.set_xlabel('Prediction')
+    fig.tight_layout()
+    return ax
+
+
+def _compute_cm(result, labels, **kwargs):
+    if 'Y_PRED' not in result.columns:
+        idx = np.argmax(result[labels].values, axis=1)
+        y_pred = [labels[i] for i in idx]
+    else:
+        y_pred = result['Y_PRED']
+    return confusion_matrix(result['Y_TRUE'], y_pred, labels=labels, **kwargs)
+
+
+def plot_roc(result, pos_label=None, title='ROC', cmap=None, ax=None):
+    '''Plot ROC curve.
+
+    Parameters
+    ----------
+    result : pandas.DataFrame
+        data frame containing predictions per sample (in row). It must have a column of
+        true class named "Y_TRUE" and multiple columns of predicted probabilities for each class.
+        It typically takes the output of :func:`classify`.
+    pos_label : str, optional
+        the interested class if it is a binary classification. For
+        example, for "Health" vs. "IBD" classification, you would want
+        to set it to "IBD". This is ignored if it is not a binary
+        classification. This value is passed to
+        :func:`sklearn.metrics.roc_curve`
+    title : str
+        plot title
+    cmap : str or matplotlib.colors.ListedColormap
+        str to indicate the colormap name. Default is "Dark2" colormap.
+        For all available colormaps in matplotlib: https://matplotlib.org/users/colormaps.html
+    ax : matplotlib.axes.Axes or None (default), optional
+        The axes where to plot. None (default) to create a new figure and
+        axes to plot
+
+    Returns
+    -------
+    matplotlib.axes.Axes
+        The axes for the ROC
+
+    '''
+    from matplotlib import pyplot as plt
+    if cmap is None:
+        cmap = plt.cm.Dark2
+    if ax is None:
+        fig, ax = plt.subplots()
+
+    ax.axis('equal')
+    ax.plot([0, 1], [0, 1], linestyle='-', lw=1, color='black', label='Luck', alpha=.5)
+
+    classes = result['Y_TRUE'].unique()
+    # if this is a binary classification, we only need to set one class as positive
+    # and just plot ROC for the positive class
+    if len(classes) == 2:
+        if pos_label is None:
+            # by default use whatever the first class is as positive
+            classes = classes[:1]
+        else:
+            classes = [pos_label]
+    col = dict(zip(classes, itertools.cycle(cmap.colors)))
+
+    mean_fpr = np.linspace(0, 1, 100)
+    for cls in classes:
+        tprs = []
+        aucs = []
+        for grp, df in result.groupby('CV'):
+            y_true = df['Y_TRUE'].values == cls
+            fpr, tpr, thresholds = roc_curve(y_true, df[cls])
+            mean_tpr = interp(mean_fpr, fpr, tpr)
+            tprs.append(mean_tpr)
+            tprs[-1][0] = 0.0
+            roc_auc = auc(mean_fpr, mean_tpr)
+            aucs.append(roc_auc)
+
+        mean_tpr = np.mean(tprs, axis=0)
+        mean_tpr[-1] = 1.0
+        mean_auc = np.mean(aucs)
+        std_auc = np.std(aucs)
+        ax.plot(mean_fpr, mean_tpr, color=col[cls],
+                label='{0} ({1:.2f} $\pm$ {2:.2f})'.format(cls, mean_auc, std_auc),
+                lw=2, alpha=.8)
+
+        std_tpr = np.std(tprs, axis=0)
+        tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
+        tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
+        ax.fill_between(mean_fpr, tprs_lower, tprs_upper, color=col[cls], alpha=.5)
+        ax.set_xlim([-0.05, 1.05])
+        ax.set_ylim([-0.05, 1.05])
+        ax.set_xlabel('False Positive Rate')
+        ax.set_ylabel('True Positive Rate')
+        ax.set_title(title)
+        ax.legend(loc="lower right")
+
+    return ax
 
 
 @Experiment._record_sig
