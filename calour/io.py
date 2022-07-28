eaacc9aa4d31f93b8cc18eb6b4180082604e9b4a
amnona <amnonim@gmail.com>
2022-07-28 15:16:15 +0300

    change from len to exp.shape

--- calour/io.py
+++ calour/io.py
@@ -474,7 +474,7 @@ def read(data_file, sample_metadata_file=None, feature_metadata_file=None,
     param = ['{0!s}={1!r}'.format(k, v) for k, v in fparams.items()]
     exp._call_history = ['{0}({1})'.format('read_amplicon', ','.join(param))]
 
-    logger.info('Loaded %d samples, %d features' % (len(exp.sample_metadata), len(exp.feature_metadata)))
+    logger.info('Loaded %d samples, %d features' % (exp.shape[0], exp.shape[1]))
 
     return exp
 
