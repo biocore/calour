177180e60ed3c1d01c9510139cac3c3357aa9c8a
amnona <amnonim@gmail.com>
2024-08-25 16:56:54 +0300

    include CorrelationExperiment in __init__ and update changelog and version

--- calour/__init__.py
+++ calour/__init__.py
@@ -15,15 +15,16 @@ from .amplicon_experiment import AmpliconExperiment
 from .correlation_experiment import CorrelationExperiment
 from .ms1_experiment import MS1Experiment
 from .mrna_experiment import mRNAExperiment
-from .io import read, read_amplicon, read_ms, read_qiime2
+from .io import read, read_amplicon, read_ms, read_qiime2, read_correlation
 from .util import set_log_level, register_functions
 
 
 __credits__ = "https://github.com/biocore/calour/graphs/contributors"
-__version__ = "2024.5.30"
+__version__ = "2024.8.25"
 
 __all__ = ['read', 'read_amplicon', 'read_ms', 'read_qiime2', 'read_correlation',
            'Experiment', 'AmpliconExperiment', 'MS1Experiment','mRNAExperiment',
+           'CorrelationExperiment',
            'set_log_level']
 
 
